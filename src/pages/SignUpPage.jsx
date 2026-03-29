395e0bb631154b461d05e94371cc1181db6b22c5
Lakha Prathamesh <prathameshlakhawork@gmail.com>
2026-03-29 19:59:06 +0530

    final

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -57,7 +57,7 @@ const SignUpPage = () => {
     setErrorMessage('')
 
     try {
-      const response = await fetch(`${API}/api/chat`, {
+      const response = await fetch(`${API}/api/auth/register`, { 
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ name: username, email, password }),
